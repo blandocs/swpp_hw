b1a5c58dad3b2775732cd38f4307a0e7cf7f783e
gustnxodjs <gustnxodjs@snu.ac.kr>
2015-04-09 00:00:27 +0900

    before security

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+	resources :comments
+	end
   get 'home/index'
   root :to => "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
